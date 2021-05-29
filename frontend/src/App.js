18afad768755803d7c53f2ef08031a3df5a20193
withsang <jsa5115@naver.com>
2021-05-30 03:52:39 +0900

    Find when the bug happens in App.js (to be fixed)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,15 +14,33 @@ import NotFoundPage from './pages/NotFoundPage';
 function App() {
   const [user, setUser] = useState(null);
   let authenticated = user !== null;
-  
   const login = ({userID, password}) => {
+  
+    // // #1 잘 동작하는 코드
+    // setUser({name: 'testName', userID: 'testID'});
+
+    // // #2 Promise를 넣었더니 로그인이 안 됨
+    
+    // return new Promise((resolve) => {
+    //     setTimeout(() => {
+    //       resolve();
+    //     }, 1000);
+    //   }).then(()=>{
+    //     setUser({name: 'testNme', userID: 'testID'})
+    //   });
+
+    // #3 마찬가지로 Promise를 써야 하는 api에서 로그인이 안 됨
     axios.post("api/user/signin", {userID, password})
     .then((response) => {
       const user = response.data;
       if (user) {
         setUser(user);
+        alert('로그인 되었습니다.');
+      } else {
+        throw new Error('로그인 실패!');
       }
     });
+
   };
 
   const logout = () => setUser(null);
