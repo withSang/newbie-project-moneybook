3d8195dd2aa0c5570cea2a8ff699225f3c962530
withsang <jsa5115@naver.com>
2021-05-30 04:16:08 +0900

    Fix login bug when async is used.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-import axios from 'axios';
 
 import AuthRoute from './routes/AuthRoute';
 import Profile from './components/Profile';
@@ -14,34 +13,6 @@ import NotFoundPage from './pages/NotFoundPage';
 function App() {
   const [user, setUser] = useState(null);
   let authenticated = user !== null;
-  const login = ({userID, password}) => {
-  
-    // // #1 잘 동작하는 코드
-    // setUser({name: 'testName', userID: 'testID'});
-
-    // // #2 Promise를 넣었더니 로그인이 안 됨
-    
-    // return new Promise((resolve) => {
-    //     setTimeout(() => {
-    //       resolve();
-    //     }, 1000);
-    //   }).then(()=>{
-    //     setUser({name: 'testNme', userID: 'testID'})
-    //   });
-
-    // #3 마찬가지로 Promise를 써야 하는 api에서 로그인이 안 됨
-    axios.post("api/user/signin", {userID, password})
-    .then((response) => {
-      const user = response.data;
-      if (user) {
-        setUser(user);
-        alert('로그인 되었습니다.');
-      } else {
-        throw new Error('로그인 실패!');
-      }
-    });
-
-  };
 
   const logout = () => setUser(null);
 
@@ -67,7 +38,7 @@ function App() {
           <Route
             exact path="/login"
             render={props => (
-              <LoginPage authenticated={authenticated} login={login} {...props} />
+              <LoginPage authenticated={authenticated} user={user} setUser={setUser} {...props} />
             )}
           />
           <Route path="/">
