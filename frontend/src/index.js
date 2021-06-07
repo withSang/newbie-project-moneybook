165457bc186a7fbcf931d16f86f7cf0cddba421e
withsang <jsa5115@naver.com>
2021-06-07 19:24:13 +0900

    Add webfont

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-datepicker/dist/react-datepicker.css";
+import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
